c341607e30a47312bed136759fbcba83c4d43eb9
jos <joheandroid@gmail.com>
2021-02-25 11:26:38 -0300

    Fix

--- dist/js/main.js
+++ dist/js/main.js
@@ -22,9 +22,9 @@ var myMenu = document.querySelector(".menu");
 
 var myMenu2 = document.querySelector(".fill");
 var oppMenu = document.querySelector(".menu-icon");
-myMenu2.addEventListener("transitionend", OnTransitionEnd, false);
-oppMenu.addEventListener("click", toggleClassMenu, false);
-myMenu.addEventListener("click", toggleClassMenu, false);
+//myMenu2.addEventListener("transitionend", OnTransitionEnd, false);
+//oppMenu.addEventListener("click", toggleClassMenu, false);
+//myMenu.addEventListener("click", toggleClassMenu, false);
 
 const navPrimary = document.querySelector(".nav--primary");
 const navSecondary = document.querySelector(".nav--secondary");
@@ -35,11 +35,13 @@ const contentWrap = document.querySelector('.content-wrap');
 
 
 $(window).bind('scroll', (function () {
-    if ($(window).scrollTop() > 50) {
-        $('.header').css('top','0px');
-		$('.header').css('margin-top','0px');
-    } else {
-        $('.header').css('top','10px');
-		$('.header').css('margin-top','26px');
-    }
+    if ($(window).width() > 768){
+		if ($(window).scrollTop() > 50) {
+			$('.header').css('top','0px');
+			$('.header').css('margin-top','0px');
+		} else {
+			$('.header').css('top','10px');
+			$('.header').css('margin-top','26px');
+		}
+	}
 }));
--- dist/js/main.polyfills.js
+++ dist/js/main.polyfills.js
@@ -29,24 +29,23 @@ var myMenu = document.querySelector(".menu");
 
 var myMenu2 = document.querySelector(".fill");
 var oppMenu = document.querySelector(".menu-icon");
-myMenu2.addEventListener("transitionend", OnTransitionEnd, false);
+//myMenu2.addEventListener("transitionend", OnTransitionEnd, false);
 oppMenu.addEventListener("click", toggleClassMenu, false);
-myMenu.addEventListener("click", toggleClassMenu, false);
+//myMenu.addEventListener("click", toggleClassMenu, false);
 
-const navPrimary = document.querySelector(".nav--primary");
-const navSecondary = document.querySelector(".nav--secondary");
-const stickyWrap = document.querySelector(".sticky-wrap");
-const contentWrap = document.querySelector('.content-wrap');
 
 
 
 
 $(window).bind('scroll', (function () {
-    if ($(window).scrollTop() > 50) {
-        $('.header').css('top','0px');
-		$('.header').css('margin-top','0px');
-    } else {
-        $('.header').css('top','10px');
-		$('.header').css('margin-top','26px');
-    }
+    if ($(window).width() > 768){
+		if ($(window).scrollTop() > 50) {
+			$('.header').css('top','0px');
+			$('.header').css('margin-top','0px');
+		} else {
+			$('.header').css('top','10px');
+			$('.header').css('margin-top','26px');
+		}
+	}
 }));
+
